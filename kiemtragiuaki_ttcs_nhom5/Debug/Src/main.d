163737c3f9dda310ee432d397407ad6d1e60cfd9
BaoNguyen247 <n21dcvt007@student.ptithcm.edu.vn>
2024-04-06 08:45:04 +0700

    My first commit

---
+++ kiemtragiuaki_ttcs_nhom5/Debug/Src/main.d
@@ -0,0 +1,31 @@
+Src/main.o: ../Src/main.c \
+ D:/Thuc\ tap\ co\ so/kiemtragiuaki_ttcs_nhom5/thuvien/middle/sensor/temhumsensor.h \
+ D:/Thuc\ tap\ co\ so/kiemtragiuaki_ttcs_nhom5/thuvien/middle/ucglib/Ucglib.h \
+ D:/Thuc\ tap\ co\ so/kiemtragiuaki_ttcs_nhom5/thuvien/middle/ucglib/ucg.h \
+ D:/Thuc\ tap\ co\ so/kiemtragiuaki_ttcs_nhom5/thuvien/middle/rtos/timer.h \
+ D:/Thuc\ tap\ co\ so/kiemtragiuaki_ttcs_nhom5/thuvien/driver/stm32driver/inc/stm32f401re_i2c.h \
+ D:/Thuc\ tap\ co\ so/kiemtragiuaki_ttcs_nhom5/thuvien/driver/Cmsis/stm32f401re.h \
+ D:/Thuc\ tap\ co\ so/kiemtragiuaki_ttcs_nhom5/thuvien/driver/Cmsis/core_cm4.h \
+ D:/Thuc\ tap\ co\ so/kiemtragiuaki_ttcs_nhom5/thuvien/driver/Cmsis/core_cmInstr.h \
+ D:/Thuc\ tap\ co\ so/kiemtragiuaki_ttcs_nhom5/thuvien/driver/Cmsis/core_cmFunc.h \
+ D:/Thuc\ tap\ co\ so/kiemtragiuaki_ttcs_nhom5/thuvien/driver/Cmsis/core_cmSimd.h \
+ D:/Thuc\ tap\ co\ so/kiemtragiuaki_ttcs_nhom5/thuvien/driver/Cmsis/system_stm32f4xx.h \
+ D:/Thuc\ tap\ co\ so/kiemtragiuaki_ttcs_nhom5/thuvien/driver/stm32driver/inc/stm32f401re_spi.h \
+ D:/Thuc\ tap\ co\ so/kiemtragiuaki_ttcs_nhom5/thuvien/driver/stm32driver/inc/stm32f401re_gpio.h \
+ D:/Thuc\ tap\ co\ so/kiemtragiuaki_ttcs_nhom5/thuvien/driver/stm32driver/inc/stm32f401re_rcc.h \
+ D:/Thuc\ tap\ co\ so/kiemtragiuaki_ttcs_nhom5/thuvien/middle/sensor/lightsensor.h
+D:/Thuc\ tap\ co\ so/kiemtragiuaki_ttcs_nhom5/thuvien/middle/sensor/temhumsensor.h:
+D:/Thuc\ tap\ co\ so/kiemtragiuaki_ttcs_nhom5/thuvien/middle/ucglib/Ucglib.h:
+D:/Thuc\ tap\ co\ so/kiemtragiuaki_ttcs_nhom5/thuvien/middle/ucglib/ucg.h:
+D:/Thuc\ tap\ co\ so/kiemtragiuaki_ttcs_nhom5/thuvien/middle/rtos/timer.h:
+D:/Thuc\ tap\ co\ so/kiemtragiuaki_ttcs_nhom5/thuvien/driver/stm32driver/inc/stm32f401re_i2c.h:
+D:/Thuc\ tap\ co\ so/kiemtragiuaki_ttcs_nhom5/thuvien/driver/Cmsis/stm32f401re.h:
+D:/Thuc\ tap\ co\ so/kiemtragiuaki_ttcs_nhom5/thuvien/driver/Cmsis/core_cm4.h:
+D:/Thuc\ tap\ co\ so/kiemtragiuaki_ttcs_nhom5/thuvien/driver/Cmsis/core_cmInstr.h:
+D:/Thuc\ tap\ co\ so/kiemtragiuaki_ttcs_nhom5/thuvien/driver/Cmsis/core_cmFunc.h:
+D:/Thuc\ tap\ co\ so/kiemtragiuaki_ttcs_nhom5/thuvien/driver/Cmsis/core_cmSimd.h:
+D:/Thuc\ tap\ co\ so/kiemtragiuaki_ttcs_nhom5/thuvien/driver/Cmsis/system_stm32f4xx.h:
+D:/Thuc\ tap\ co\ so/kiemtragiuaki_ttcs_nhom5/thuvien/driver/stm32driver/inc/stm32f401re_spi.h:
+D:/Thuc\ tap\ co\ so/kiemtragiuaki_ttcs_nhom5/thuvien/driver/stm32driver/inc/stm32f401re_gpio.h:
+D:/Thuc\ tap\ co\ so/kiemtragiuaki_ttcs_nhom5/thuvien/driver/stm32driver/inc/stm32f401re_rcc.h:
+D:/Thuc\ tap\ co\ so/kiemtragiuaki_ttcs_nhom5/thuvien/middle/sensor/lightsensor.h:
